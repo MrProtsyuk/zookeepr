f9c354a937939d658aed4f5740fa046d6c3d6a79
Your Name <markprotsyuk@Marks-MacBook-Pro-2.local>
2022-03-05 09:49:28 -0800

    add front end files and implemented fetch into script.js

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`API server now on port 3001!`);
 });
